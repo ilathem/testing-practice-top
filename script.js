ecc252adfac24801835ad07ce625e6378f7afd53
ilathem <isaiahlathem@gmail.com>
2024-06-20 19:09:20 -0500

    add reverseString function

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-module.exports = { capitalize };
+function reverseString(string) {
+  let newStr = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    newStr += (string.charAt(i));
+  }
+  return newStr;
+}
+
+module.exports = { capitalize, reverseString };
