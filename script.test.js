3f5d14545f798ed0640924c9ca5a939e0cfd34f5
ilathem <isaiahlathem@gmail.com>
2024-06-20 19:14:55 -0500

    add test for calculator

--- script.test.js
+++ script.test.js
@@ -8,3 +8,10 @@ test('Reverse String', () => {
   expect(reverseString('abcd')).toMatch(/dcba/);
   expect(reverseString('cat')).toMatch(/tac/);
 });
+
+test('Calculator', () => {
+  expect(calculator.add(1, 2)).toEqual(3);
+  expect(calculator.subtract(5, 2)).toEqual(3);
+  expect(calculator.multiply(6, 8)).toEqual(48);
+  expect(calculator.divide(121, 11)).toEqual(1/*  */1);
+})
