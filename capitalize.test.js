204373dcfb74f69fd59d5a9f9efe967401ac0f25
ilathem <isaiahlathem@gmail.com>
2024-06-20 19:00:26 -0500

    Write capitalize function

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,5 @@
 const capitalize = require('./capitalize');
 
 test('Capitalize', () => {
-  expect(capitalize('hello world').toMatch(/Hello world/))
+  expect(capitalize('hello world')).toMatch(/Hello world/)
 })
