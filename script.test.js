5d8fab1cca59266ea639671b87c6dae368289fba
ilathem <isaiahlathem@gmail.com>
2024-06-20 19:05:46 -0500

    add reverseString test

--- script.test.js
+++ script.test.js
@@ -1,5 +1,10 @@
-const { capitalize } = require('./script');
+const { capitalize, reverseString } = require('./script');
 
 test('Capitalize', () => {
-  expect(capitalize('hello world')).toMatch(/Hello world/)
-})
+  expect(capitalize('hello world')).toMatch(/Hello world/);
+});
+
+test('Reverse String', () => {
+  expect(reverseString('abcd')).toMatch(/dcba/);
+  expect(reverseString('cat')).toMatch(/tac/);
+});
