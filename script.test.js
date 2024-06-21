5249d631ed691d78116313899a419ca2d785dbf9
ilathem <isaiahlathem@gmail.com>
2024-06-20 19:19:14 -0500

    add caesarCipher tests

--- script.test.js
+++ script.test.js
@@ -15,3 +15,9 @@ test('Calculator', () => {
   expect(calculator.multiply(6, 8)).toEqual(48);
   expect(calculator.divide(121, 11)).toEqual(11);
 })
+
+test('Caesar Cipher', () => {
+  expect(caesarCipher('xyz', 3)).toMatch(/abc/);
+  expect(caesarCipher('HeLLo', 3)).toMatch(/KhOOr/);
+  expect(caesarCipher('Hello, World!', 3)).toMatch('Khoor, Zruog!');
+})
