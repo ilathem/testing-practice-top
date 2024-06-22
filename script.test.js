7ce15c81ab97ade76d5181ebfb367e00a623f59e
Isaiah Lathem <isaiahlathem@gmail.com>
2024-06-22 08:46:13 -0500

    Add analyze array test

--- script.test.js
+++ script.test.js
@@ -3,26 +3,36 @@ const {
   reverseString,
   calculator,
   caesarCipher,
-} = require('./script');
+} = require("./script");
 
-test('Capitalize', () => {
-  expect(capitalize('hello world')).toMatch(/Hello world/);
+test("Capitalize", () => {
+  expect(capitalize("hello world")).toMatch(/Hello world/);
 });
 
-test('Reverse String', () => {
-  expect(reverseString('abcd')).toMatch(/dcba/);
-  expect(reverseString('cat')).toMatch(/tac/);
+test("Reverse String", () => {
+  expect(reverseString("abcd")).toMatch(/dcba/);
+  expect(reverseString("cat")).toMatch(/tac/);
 });
 
-test('Calculator', () => {
+test("Calculator", () => {
   expect(calculator.add(1, 2)).toEqual(3);
   expect(calculator.subtract(5, 2)).toEqual(3);
   expect(calculator.multiply(6, 8)).toEqual(48);
   expect(calculator.divide(121, 11)).toEqual(11);
 });
 
-test('Caesar Cipher', () => {
-  expect(caesarCipher('xyz', 3)).toMatch(/abc/);
-  expect(caesarCipher('HeLLo', 3)).toMatch(/KhOOr/);
-  expect(caesarCipher('Hello, World!', 3)).toMatch('Khoor, Zruog!');
+test("Caesar Cipher", () => {
+  expect(caesarCipher("xyz", 3)).toMatch(/abc/);
+  expect(caesarCipher("HeLLo", 3)).toMatch(/KhOOr/);
+  expect(caesarCipher("Hello, World!", 3)).toMatch("Khoor, Zruog!");
 });
+
+test("Analyze Array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
